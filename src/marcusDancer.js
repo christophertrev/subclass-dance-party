481eea87b35a2650b7acc93377508a20c4650f41
christophertrev <christophertrev@gmail.com>
2014-11-04 15:41:13 -0800

    Refactored syntax for marcusdancer

--- src/marcusDancer.js
+++ src/marcusDancer.js
@@ -1,10 +1,10 @@
 var MarcusDancer = function(top, left, timeBetweenSteps){
 
     this.stepSize = 0.01;
-    Dancer.call(this,top, left, timeBetweenSteps);
+    Dancer.call(this, top, left, timeBetweenSteps);
     this.$node.addClass('marcus');
-    this.$node.mouseover( function (event){
-     this.timeBetweenSteps *= 0.5;
+    this.$node.mouseover(function (event){
+      this.timeBetweenSteps *= 0.5;
     }.bind(this));
 
 };
@@ -12,15 +12,11 @@ var MarcusDancer = function(top, left, timeBetweenSteps){
 MarcusDancer.prototype = Object.create(Dancer.prototype);
 MarcusDancer.prototype.constructor = MarcusDancer;
 
-
 MarcusDancer.prototype.step = function(){
-  // call the old version of step at the beginning of any call to this new version of step
-  //oldStep();
   Dancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  //this.$node.toggle();
   this.top += this.stepSize*($("body").height() * (Math.random()-.5));
   this.left += this.stepSize*($("body").width() * (Math.random()-.5));
   this.setPosition();
