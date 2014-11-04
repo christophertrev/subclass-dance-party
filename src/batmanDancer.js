6777f8d43ed2b07a5b9e508bf41d6fef3dcac2c8
christophertrev <christophertrev@gmail.com>
2014-11-04 15:48:51 -0800

    Refactored BatmanDancer for syntax and setposition (removed inputs).

--- src/batmanDancer.js
+++ src/batmanDancer.js
@@ -1,23 +1,19 @@
 var BatmanDancer = function(top, left, timeBetweenSteps){
-    this.timeBetweenSteps = 3000;
-    Dancer.call(this,top, left, this.timeBetweenSteps);
-    this.$node.addClass('batman')
+  this.timeBetweenSteps = 3000;
+  Dancer.call(this, top, left, this.timeBetweenSteps);
+  this.$node.addClass('batman');
 };
 
 BatmanDancer.prototype = Object.create(Dancer.prototype);
 BatmanDancer.prototype.constructor = BatmanDancer;
 
-
 BatmanDancer.prototype.step = function(){
-  // call the old version of step at the beginning of any call to this new version of step
-  //oldStep();
   Dancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  //this.$node.toggle();
   this.$node.fadeToggle(this.timeBetweenSteps/2).fadeToggle(this.timeBetweenSteps/2);
   this.top = $("body").height() * Math.random();
   this.left = $("body").width() * Math.random();
-  this.setPosition(this.top,this.left)
+  this.setPosition();
 };
